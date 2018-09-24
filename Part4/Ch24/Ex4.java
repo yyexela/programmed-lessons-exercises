399ab5fa2ae067976fecf29e7cca59ebe62d6bcd
Alexey Yermakov <alexey@epmaki.net>
2018-09-24 07:52:56 -0600

    Ex4.java now uses FOR loops

--- Part4/Ch24/Ex4.java
+++ Part4/Ch24/Ex4.java
@@ -5,20 +5,14 @@ class Ex4{
         Scanner scan = new Scanner(System.in);
         System.out.println("Input N: ");
         int N = scan.nextInt();
-        int a = 0, b = 0;
         boolean square = false;
-        while(a*a < N){
-            while(((a * a) + (b * b)) < N){
-                b++;
+        for(int a = 0; a*a <= N; a++){
+            for(int b = 0; ((a * a) + (b * b)) <= N; b++){
+                if((a * a) + (b * b) == N){
+                    System.out.println("Perfect square of N is: " + a + "^2 + " + b + "^2");
+                    square = true;
+                }           
             }
-            //System.out.println("a = " + a + " b = " + (b));
-            //System.out.println("a^2 + b^2 = " + ((a * a) + (b * b)));
-            if((a * a) + (b * b) == N){
-                System.out.println("Perfect square of N is: " + a + "^2 + " + b + "^2");
-                square = true;
-            }
-            b=0;
-            a++;
         }
         if(!square){
             System.out.println(N + " is not a perfect square");
