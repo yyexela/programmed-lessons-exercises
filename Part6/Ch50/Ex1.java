9b11cb115a584b20b3a37bf32cbaed676d0b6d01
Alexey Yermakov <alexey@epmaki.net>
2018-10-25 08:38:02 -0600

    Ch50/Ex1.java formatting

--- Part6/Ch50/Ex1.java
+++ Part6/Ch50/Ex1.java
@@ -3,7 +3,6 @@ public class Ex1{
         Box box = new Box(2.5, 5.0, 6.0) ;
 
         System.out.println( "Area: "  + box.area() + " volume: " + box.volume() );
-   
         System.out.println( "length: " + box.getLength() + " height: " + box.getHeight() +" width: " + box.getWidth() )  ;
     }
 }
