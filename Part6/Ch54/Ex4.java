ee07819edd34d406c7dac5fe64e109664dee6f30
Alexey Yermakov <alexey@epmaki.net>
2018-10-30 08:22:38 -0600

    Ch54/Ex4.java fixed to work correctly + user input for max

--- Part6/Ch54/Ex4.java
+++ Part6/Ch54/Ex4.java
@@ -1,32 +1,61 @@
-
+import java.util.Scanner;
 
 public class Ex4{
     public static void main(String[] args) {
-        int n = 28;
-        int sumFactors = 0;
-        int factorNum = 0;
+        Scanner scan = new Scanner(System.in);
 
-        if(n > 0){
-            System.out.println("numberDivisible: 1");
-            factorNum = 2;
-            sumFactors++;
+        long max = getLimit(scan);
+
+
+        System.out.println("Perfect numbers:");
+
+        /*
+        //Single test
+        if(isPerfect(max)) System.out.println("perfect");
+        else System.out.println("is not");
+        */
+
+        
+        //Multiple test
+        for(long i = 2; i <= max; i++){
+            if(isPerfect(i)){
+                if(i >= 2){
+                    System.out.println(i);
+                }
+            }
         }
 
-        while(sumFactors < n && factorNum < n){
+    }
+
+    public static long getLimit(Scanner scan){
+        System.out.println("Enter upper limit:");
+        return scan.nextLong();
+    }
+
+    public static boolean isPerfect(long n){
+        int sumFactors = 0;
+        int factorNum = 1;
+
+        if(n > 0){
+            //System.out.println("numberDivisible: 1");
+            sumFactors += factorNum;
+            factorNum++;
+        }
+
+        while(sumFactors <= n && factorNum < n){
             if(n % factorNum == 0){
-                System.out.println("numberDivisible: " + factorNum);
+                //System.out.println("numberDivisible: " + factorNum);
                 sumFactors += factorNum;
             }
             factorNum++;
-            System.out.println("Next num:" + factorNum);
+            //System.out.println("Next num:" + factorNum);
         }
 
-        System.out.println("sumFactors: " + sumFactors);
+        //System.out.println("sumFactors: " + sumFactors);
 
         if(sumFactors == n){
-            System.out.println("perfect");
-        } else System.out.println("imperfect");
-
-
+            return true;
+        } else return false;
     }
+
 }
