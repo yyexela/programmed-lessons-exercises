33ff53a98ece18f813ea618796956636ca9188da
Alexey Yermakov <alexey@epmaki.net>
2018-11-14 08:18:14 -0700

    Ex2.java works properly

---
+++ Part7/Ch60/Ex2.java
@@ -0,0 +1,22 @@
+public class Ex2{
+    public static void main(String[] args) {
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice;
+        
+        System.out.println( "Original Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+    
+        // Construct an array object for twice.
+        twice = new int[4];
+        
+        // Put values in twice that are twice the
+        // corresponding values in val.
+        for(int i = 0; i < val.length; i++){
+            twice[i] = 2 * val[i];
+        }
+
+    
+        System.out.println( "New Array: " 
+            + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+    }
+}
