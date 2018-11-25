a12c4c1a74bc3da70130bcc56a36914d7d0549b2
Alexey Yermakov <alexey@epmaki.net>
2018-11-25 12:43:29 -0700

    Weights.java and WeightTexter.java work with Ex1

---
+++ Part7/Ch62/Weight.java
@@ -0,0 +1,25 @@
+public class Weight{
+  private int[] data;
+  
+  // Constructor
+  public Weight(int[] init){
+        // Make data the same length
+        // as the array referenced by init.
+        data = new int[init.length];
+        
+        // Copy values from the 
+        // input data to data.
+        for (int i = 0; i < data.length; i++){
+            data[i] = init[i];
+        }
+    }
+    
+    //Print
+    public String toString(){
+        String complete = "";
+        for(int i = 0; i < data.length; i++){
+            complete += data[i] + ", ";
+        }
+        return complete;
+    }
+}
